42a937f71443b8b327a45551ccf7a10304f9032b
Nick Mills <nick.mills94563@gmail.com>
2026-05-29 23:23:30 -0400

    Remove unused user routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,7 +2,6 @@ import express, { type Request, type Response} from 'express';
 import { registerUser, loginUser } from "@/backend/controllers/authController.js";
 
 const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET as string;
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
 import { getAllUsers } from '../controllers/userController.js';
-import { getUserActivities } from '../controllers/activityController.js';
-import { getUserHours } from '../controllers/timeController.js';
 
 const router = Router({ mergeParams: true });
 
 router.get('/', getAllUsers);
-
-router.get('/:userId/activities', getUserActivities);
-
-router.get('/:userId/hours', getUserHours);
 
 export default router;
