f336ded3fe6c724e1752e9a5767479f4cb8a6ace
Nick Mills <carlos94563@gmail.com>
2026-05-31 08:55:58 -0400

    Work on fully containerization of the entire app: backend, frontend, and database.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,14 +6,17 @@ set -e
 echo "Waiting for PostgreSQL container to be fully ready..."
 
 # Loop until the PostgreSQL container is ready
-until npx prisma db validate --schema=./prisma/schema.prisma > /dev/null 2>&1; do
+until pg_isready -h postgres_db -p 5432 -U postgres; do
   sleep 1
 done
 
 echo "PostgreSQL is ready! Executing clean table wipe and schema sync..."
 
 # Run the Prisma migration to sync the schema
-npx prisma migrate reset --force --schema=./prisma/schema.prisma
+npm run db:rebuild
+
+echo "Seeding database with some data..."
+npm run db:seed --schema=./prisma/schema.prisma
 
 echo "Database build sequence complete! Launching Express server..."
 
